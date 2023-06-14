d23985919df7b19ad91bbf8ed05e36b6a51d0557
okatsn <okatsn@gmail.com>
2023-06-14 18:00:26 +0800

    Clean corpus

--- scripts/convert_json_1.jl
+++ scripts/convert_json_1.jl
@@ -1,7 +1,15 @@
 using JSON, CorpusCleanerForTWLaws
 using ProjectLawMaster
+using StructArrays
 
 rawcorpus = JSON.parsefile(ProjectLawMaster.project_dir("corpus", "judicial_yuan_qa.json"))
 
-#
+
 vcat([keys(d) for d in rawcorpus]...) |> unique
+
+
+cps = [CorpusJudicalYuan(d) for d in rawcorpus]
+
+soacps = StructArray(cps)
+
+clean!.(cps)
