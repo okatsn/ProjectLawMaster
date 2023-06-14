c124260f209e1e912f1c1a8f5b2c357b786263ef
okatsn <okatsn@gmail.com>
2023-06-14 17:16:42 +0800

    Knowing the keys

--- scripts/convert_json_1.jl
+++ scripts/convert_json_1.jl
@@ -1,3 +1,7 @@
-using JSON
-JSON.parsefile("corpus/judical_yuan_qa.json")
-pwd()
+using JSON, CorpusCleanerForTWLaws
+using ProjectLawMaster
+
+rawcorpus = JSON.parsefile(ProjectLawMaster.project_dir("corpus", "judicial_yuan_qa.json"))
+
+#
+vcat([keys(d) for d in rawcorpus]...) |> unique
