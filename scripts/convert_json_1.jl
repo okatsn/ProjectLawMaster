94e994139bbc7261fda9abb44831281515a9a187
okatsn <okatsn@gmail.com>
2023-06-15 17:09:58 +0800

    Corpus of JudicalYuan cleaned and tests passed

--- scripts/convert_json_1.jl
+++ scripts/convert_json_1.jl
@@ -1,18 +1,31 @@
 
 # Please activate the environment of ProjectLawMaster first.
-
 using JSON, CorpusCleanerForTWLaws
 using ProjectLawMaster
-using StructArrays
+using Test
 
-rawcorpus = JSON.parsefile(ProjectLawMaster.project_dir("corpus", "judicial_yuan_qa.json"))
+file0 = ProjectLawMaster.project_dir("corpus", "judicial_yuan_qa.json")
+file1 = ProjectLawMaster.project_dir("corpus", "judicial_yuan_qa_CLEANED.json")
 
-
-vcat([keys(d) for d in rawcorpus]...) |> unique
-
+# # Load corpus as a CorpusType
+rawcorpus = JSON.parsefile(file0)
 
 cps = [CorpusJudicalYuan(d) for d in rawcorpus]
 
-soacps = StructArray(cps)
-
+# # Clean the corpus
 clean!.(cps)
+
+# # Save results as JSON file
+open(file1, "w") do io
+    CorpusCleanerForTWLaws.print(io, cps)
+end
+
+# # Test if JSON file is correctly saved
+@testset "test results" begin
+    rawcorpus1 = JSON.parsefile(file1)
+    for (c0, c1) in zip(cps, rawcorpus1)
+        @test c0.question == c1["instruction"]
+        @test c0.answer == c1["output"]
+        @test isempty(c1["input"])
+    end
+end
